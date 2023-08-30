40396f86b5cbeaae0f068fa05c8ee61bc0df0816
mcspe <mcspezzaferro@gmail.com>
2023-08-30 21:59:41 +0200

    [add] home and detail pages

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import SearchBar from '../components/SearchBar.vue';
+  import { store } from '../store/store';
+</script>
+
+<template>
+  <div class="container mx-auto">
+    <SearchBar class="flex md:hidden" />
+    <div>
+      <ul v-if="store.isLoaded">
+        <li v-for="car in store.apiResults" :key="car.id">
+          <router-link :to="{name: 'detail', params:{ id: car.id }}">
+            <span>{{ car.make }}</span>
+            <span>{{ car.model }}</span>
+            <span>{{ car.price }}</span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
