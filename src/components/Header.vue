b4b406a71786f6b10198480f038643f090c5a8fc
mcspe <mcspezzaferro@gmail.com>
2023-08-30 21:58:51 +0200

    [add] basic header setup

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup>
+  // import { router } from '../router';
+  import SearchBar from './SearchBar.vue'
+</script>
+
+<template>
+  <header class="container mx-auto py-4 flex justify-center md:justify-between items-center">
+    <router-link :to="{name: 'home'}" class="cursor-pointer">
+      <img src="/logo.png" alt="LOGO" class="object-cover h-14">
+    </router-link>
+    <SearchBar class="hidden md:flex"/>
+  </header>
+</template>
